8849ff90b36f96c394a0b5b18f20760ffcf924ad
OrbisCorporation <mrjanzx@gmail.com>
2018-09-24 21:45:59 -0500

    RoundRobin

--- main.cpp
+++ main.cpp
@@ -1,119 +1,50 @@
-#include <windows.h>
-#include <objidl.h>
-#include <gdiplus.h>
-using namespace Gdiplus;
-#pragma comment (lib,"Gdiplus.lib")
+#include<iostream>
+#include<string>
 
-VOID OnPaint(HDC hdc)
+using namespace std;
+
+struct Node {
+	int id;
+	string name;
+	int processTime;
+	Node * next;
+	bool bDelete;
+};
+
+void showElapsedTime(const int elapsedTime);
+void burstNodeTime(Node * item, int & elapsedTime, const int queueTime);
+void showQueueTime(int & time);
+void pushBack(Node *& lista);
+Node * popFront(Node *& lista);
+
+int main() 
 {
-	Graphics graphics(hdc);
-	Pen      pen(Color(255, 0, 0, 255));
-	graphics.DrawLine(&pen, 0, 0, 200, 100);
+	
+	return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
+void showElapsedTime(const int elapsedTime) 
 {
+	cout << "\nTiempo transcurrido: "<<elapsedTime<<" /s"; 
+}
 
-	//================ Variables del sistema ===================
-
-	HWND                hWnd;
-	MSG                 msg;
-	WNDCLASS            wndClass;
-	GdiplusStartupInput gdiplusStartupInput;
-	ULONG_PTR           gdiplusToken;
-
-	//================ fin Variables del sistema ===================
-
-	//================ Inicializa GDI+ ===================
-
-	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
-	//================ Fin inicializa GDI+ ===================
-
-	//================ Parametros de la ventana ===================
-
-	wndClass.style = CS_HREDRAW | CS_VREDRAW;
-	wndClass.lpfnWndProc = WndProc;
-	wndClass.cbClsExtra = 0;
-	wndClass.cbWndExtra = 0;
-	wndClass.hInstance = hInstance;
-	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wndClass.lpszMenuName = NULL;
-	wndClass.lpszClassName = TEXT("GettingStarted");
-
-	//================ Fin parametros de la ventana ===================
-
-	//================ Registra la clase de la ventana ===================
-
-	RegisterClass(&wndClass);
-
-	//================ Fin registra clase de la ventana ===================
-
-	//================ Crea la ventana de la clase registrada ===================
-
-	hWnd = CreateWindow(
-		TEXT("GettingStarted"),   // window class name
-		TEXT("Getting Started"),  // window caption
-		WS_OVERLAPPEDWINDOW,      // window style
-		CW_USEDEFAULT,            // initial x position
-		CW_USEDEFAULT,            // initial y position
-		CW_USEDEFAULT,            // initial x size
-		CW_USEDEFAULT,            // initial y size
-		NULL,                     // parent window handle
-		NULL,                     // window menu handle
-		hInstance,                // program instance handle
-		NULL);                    // creation parameters
-
-	//================ Fin crea la ventana de la clase registrada ===================
-
-	//================ Muestra y actualiza la ventana ===================
-
-	ShowWindow(hWnd, iCmdShow);
-	UpdateWindow(hWnd);
-
-	//================ Fin de muestra y actualiza la ventana ===================
-
-	//================ Ciclo de mensajes de Windows ===================
-
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
-	}
-
-	//================ Fin de ciclo de mensajes de Windows ===================
-
-	//================ Libera GDI+ ===================
-
-	GdiplusShutdown(gdiplusToken);
-
-	//================ Fin Libera GDI+ ===================
-
-	return msg.wParam;
-}  // WinMain
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
-	WPARAM wParam, LPARAM lParam)
+void burstNodeTime(Node * item, int & elapsedTime, const int queueTime) 
 {
-	HDC          hdc;
-	PAINTSTRUCT  ps;
-
-	switch (message)
+	if (queueTime >= item->processTime)
 	{
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		OnPaint(hdc);3
-		EndPaint(hWnd, &ps);
-		return 0;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
+		elapsedTime += item->processTime;
+		item->bDelete = true;
 	}
-} // WndProc
+	else 
+	{
+		item->processTime -= queueTime;
+		elapsedTime += queueTime;
+	}
+}
 
+void showQueueTime(int & time) {
+	cout << "\nTiempo Quantum: " << time << " /s";
+}
+
+void pushBack(Node *& lista);
+Node * popFront(Node *& lista);
